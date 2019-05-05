040cb7d9cdf9906624095fbd391f109aba51cabf
gaylonalfano <gaylon.alfano@gmail.com>
2019-05-05 20:09:52 +0800

    Added the calc.py and tests.py files for a TDD refresher. Still having issues with flake8 command. It works when using local pipenv but not when using docker-compose. I think the issue is that my local Pipfile.lock is different than the one in Docker (the venvs are different - see notes). So, saving changes now and then next debugging attempt is to uninstall flake8 and DRF from local pipenv virtual environment so there's no chance it is overwriting the Docker Pipfile whenever building our image and running our container.

---
+++ backend/app/calc.py
@@ -0,0 +1,11 @@
+# backend/app/calc.py
+
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract one number from another"""
+    return y - x
---
+++ backend/app/tests.py
@@ -0,0 +1,22 @@
+# backend/app/tests.py
+# Import the Django TestCase. This is a class that has a bunch of helper functions
+from django.test import TestCase
+
+# Import function we're going to test
+from app.calc import add, subtract
+
+# Create the test class we want
+
+
+class CalcTests(TestCase):
+
+    # Create the test with two components. MUST start with "test"!
+    # 1. Setup stage - Set your function up to be testing
+    # 2. Assertion - Confirm that the output is equal to what it should be
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        self.assertEqual(subtract(5, 11), 6)
